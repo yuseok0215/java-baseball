b9befafa2111f2d309342b70ef8f88d37dd7deb6
yuseok0215 <elwlahstmxjf@naver.com>
2023-09-21 21:44:25 +0900

    feat: 컴퓨터가 생각하는 숫자 3자리 무작위 선정 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,10 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
@@ -19,6 +23,19 @@ public class Application {
         }
 
         return input_number;
+    }
+
+    // 컴퓨터가 생각하는 숫자 3자리 무작위 선정 기능
+    public List<Integer> selectAnswer() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 
     public static void main(String[] args) {
