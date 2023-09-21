662e77d870d82b1d9b42fac44fce019b13ff8b6c
yuseok0215 <elwlahstmxjf@naver.com>
2023-09-21 21:42:18 +0900

    feat: 게임 시작을 알리는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
 public class Application {
+
+    // 게임 시작을 알리는 기능
+    public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+     /* 필요한 기능(역할)
+    1. 게임 시작을 알리는 기능
+    2. 숫자를 입력하는 기능(문자 길이에 따른 예외처리 필요)
+    3. 컴퓨터가 생각하는 숫자 3자리 무작위 선정 기능
+    4. 볼과 스트라이크의 유무를 확인하는 기능
+    5. 볼과 스트라이크의 결과에 따른 구문 출력하는 기능
+    */
+
 }
