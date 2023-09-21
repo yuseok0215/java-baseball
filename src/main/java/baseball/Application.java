e3c6e78e3cec67b7d07507dc9fc4c237c002e6c1
yuseok0215 <elwlahstmxjf@naver.com>
2023-09-21 21:43:30 +0900

    feat: 숫자를 입력하는 기능(문자 길이에 따른 예외처리 필요) 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 
     // 게임 시작을 알리는 기능
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    // 숫자를 입력하는 기능(문자 길이에 따른 예외처리 필요)
+    public String inputNumber() {
+        System.out.println("숫자를 입력해주세요 : ");
+        String input_number = Console.readLine();
+
+        if (input_number.length() != 3) {
+            throw new IllegalArgumentException("입력한 숫자의 길이는 3이어야 합니다.");
+        }
+
+        return input_number;
     }
 
     public static void main(String[] args) {
